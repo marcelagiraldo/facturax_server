7d777a04fee6a2c86e56082f448a7b326a8b46d5
marcelagiraldo <lmarceag@gmail.com>
2025-05-21 23:47:11 -0500

    Agregrando modulo de notas

--- src/services/notes.service.js
+++ src/services/notes.service.js
@@ -1,7 +1,7 @@
 import pool from "../config/db.js"
 
 export const crearNota = async (title, date) => {
-  const query = 'call proyecto.crear_nota($1,$2);';
+  const query = 'select * from proyecto.crear_nota($1,$2);';
   await pool.query(query, [title, date]);
   return { message: 'Nota creada exitosamente' };
 };
